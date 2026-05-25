f1b78e2a92d912edf0da53990fc58905a10a0ab1
Bradley McInerney <bradleymcinerney@hotmail.com>
2026-05-25 14:45:55 +1000

    Can not handle multiple steam families

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 TODO:
     Readme
         Setup instructions
-    Migrate from dataclass to pydantic
+            Libraries
+            Settings
+            APIs
+                Burner account
     Anti spam measures? 
         Max game count in 1 day, if over, ignore
         Ignore if no previous user file existed?
-    Games do not hide if hidden by the owner of the API key
-    Run with multiple sets of data (webhook and users)
-        We'd not want to make the same calls multiple times if the same users are for some reason in different sets.
-            Lazy way would be a caching library with a small (relatively, maybe an hour) lifespan, otherwise track it ourselves
+    Cache calls for 1 hour?
+    Test if private accounts will work if they are friends with the API account
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from steamApi import SteamApi, SteamUserData
 from WebhookPublisher import WebhookPublisher
+from structs import DiscordWebhook
 import os
 
 # ===============================================
@@ -8,16 +9,14 @@ import os
 
 # Steam Web API key
 API_KEY: str = os.environ.get("SteamWebApiKey")
-# The webhook to be displayed to
-DISCORD_WEBHOOK: str = ""
-# User Steam ids to watch, should be the users in your steam family.
-USERS: list[int] = [
-    76561198060418017, # Me
-    76561198915761429, # Bean
-    76561198206657967, # Tanner
-    76561198125949369, # Sasha
-    76561198323443242, # Xav
-    76561198197931509  # Tom
+
+DISCORD_WEBHOOKS = [
+    DiscordWebhook(
+        webhook_url="",
+        user_ids=[
+
+        ]
+    )
 ]
 
 # If true will not notify users, will only update csv
@@ -30,26 +29,32 @@ SILENT_MODE = False
 
 def main():
     # Basic settings validation
-    if not DISCORD_WEBHOOK: raise ValueError("DISCORD_WEBHOOK is not set")
-    if not API_KEY:         raise ValueError("API_KEY is not set")
+    if not DISCORD_WEBHOOKS: raise ValueError("DISCORD_WEBHOOKS is not set")
+    if not API_KEY:          raise ValueError("API_KEY is not set")
 
-    # Collect and update data
-    user_data: SteamUserData = SteamApi.get_user_owned_apps(API_KEY, USERS)
+    for index, hook in enumerate(DISCORD_WEBHOOKS):
+        # Collect and update data
+        user_data: SteamUserData = SteamApi.get_user_owned_apps(API_KEY, hook.user_ids)
 
-    # Optionally ship publishing
-    if SILENT_MODE:
-        return
-    
-    # Skip if no changes
-    if not sum(
-        len(user.game_data.added_app_ids) + len(user.game_data.removed_app_ids)
-        for user in user_data.steam_users
-    ):
-        print("Info: No changes!")
-        return
-    
-    # Create app data
-    WebhookPublisher.publish_update(DISCORD_WEBHOOK, API_KEY, user_data)    
+        # Optionally ship publishing
+        if SILENT_MODE:
+            continue
+        
+        # Make sure the webhook url is not empty
+        if not hook.webhook_url: 
+            print(f"Warning: Skipping DISCORD_WEBHOOKS[{index}] because it has an empty webhook_url.")
+            continue
+        
+        # Skip if no changes
+        if not sum(
+            len(user.game_data.added_app_ids) + len(user.game_data.removed_app_ids)
+            for user in user_data.steam_users
+        ):
+            print("Info: No changes!")
+            return
+        
+        # Create app data
+        WebhookPublisher.publish_update(hook.webhook_url, API_KEY, user_data)    
 
 if __name__ == "__main__":
     main()
